a986e3353c0dfec8e0e6bf9a763a83e1eb828aa2
Oscar David <delacruzhernandezoscardavid@outlook.com>
2025-10-31 23:38:59 -0600

    nuevo3

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
+
 
 
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, FooterComponent],
+  imports: [RouterModule],
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css'],
 })
