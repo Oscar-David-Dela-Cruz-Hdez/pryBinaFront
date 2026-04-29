354b31eafc26ae76fcda23d37c03ec973b623255
Oscar David De la Cruz Hernández <delacruzhernandezoscardavid@outlook.com>
2026-04-29 08:43:43 -0600

    propuesta 1.1.2

--- src/app/administrador/ventas/reportes/reportes.component.ts
+++ src/app/administrador/ventas/reportes/reportes.component.ts
@@ -8,7 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatNativeDateModule, MAT_DATE_LOCALE, DateAdapter } from '@angular/material/core';
 
 import { ProductsService } from '../../../core/services/admin/products.service';
 import { FamiliasService } from '../../../core/services/admin/familias.service';
@@ -52,7 +52,7 @@ interface MesProyeccion {
   templateUrl: './reportes.component.html',
   styleUrls: ['./reportes.component.css'],
   providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
+    { provide: MAT_DATE_LOCALE, useValue: 'es-MX' }
   ]
 })
 export class ReportesComponent implements OnInit {
@@ -169,8 +169,11 @@ export class ReportesComponent implements OnInit {
   constructor(
     private productsService: ProductsService,
     private familiasService: FamiliasService,
-    private ordersService: OrdersService
-  ) {}
+    private ordersService: OrdersService,
+    private dateAdapter: DateAdapter<Date>
+  ) {
+    this.dateAdapter.setLocale('es-MX');
+  }
 
   ngOnInit() {
     this.loadInitialData();
